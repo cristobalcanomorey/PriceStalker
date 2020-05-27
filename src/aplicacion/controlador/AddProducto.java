9b346054fbec8ccea2a57683732b6aaa59d83e47
tofol <tofolcanodaw2@gmail.com>
2020-05-27 23:46:02 +0200

    Comentarios para AddProducto y AdminUser

--- src/aplicacion/controlador/AddProducto.java
+++ src/aplicacion/controlador/AddProducto.java
@@ -31,12 +31,19 @@ public class AddProducto extends HttpServlet {
 	ProductosEJB productosEJB;
 
 	@Override
+	/****
+	 * Método GET que obtiene el usuario de la sesión y si está logueado muestra la
+	 * página de añadir producto. Si no, redirige a Principal.
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession(false);
 		LogSingleton log = LogSingleton.getInstance();
 		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		/**
+		 * Si no está logueado redirige a Principal.
+		 */
 		if (usuario != null) {
 			RequestDispatcher rs = getServletContext().getRequestDispatcher("/PaginaAddProducto.jsp");
 			request.setAttribute("nombreLista", listasEJB.getNombreLista(usuario.getId()));
@@ -50,28 +57,50 @@ public class AddProducto extends HttpServlet {
 		}
 	}
 
+	/****
+	 * Método POST que obtiene el usuario de la sesión, enlace del producto, el
+	 * nombre de la lista y el precio objetivo de los parametros. Si el usuario está
+	 * logueado y los parametros del producto no están vacíos añade un producto a la
+	 * lista. También, si el parametro del nombre de la lista no está vacío crea una
+	 * lista para el usuario.
+	 * 
+	 * Si consigue añadir un producto o crear una lista con un producto redirige a
+	 * la Lista, si algo falla redirige a Principal.
+	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession(false);
 		LogSingleton log = LogSingleton.getInstance();
+		/**
+		 * Obtiene los datos necesarios para añadir un producto o crear una lista.
+		 */
 		Usuario usuario = sesionesEJB.usuarioLogeado(session);
 		String enlace = request.getParameter("enlace");
 		String nombreLista = request.getParameter("nombreLista");
 		String precioObjetivo = request.getParameter("precioObjetivo");
+		/**
+		 * Comprueba si ha conseguido los datos necesarios, si no redirige a Principal.
+		 */
 		if (usuario != null & enlace != null & precioObjetivo != null) {
+			/**
+			 * Si obtiene el nombre de lista crea una lista para el usuario.
+			 */
 			if (nombreLista != null) {
 				listasEJB.insertLista(nombreLista, usuario.getId());
 			}
-			Integer idProducto = productosEJB.existeProducto(enlace);
+			Integer idProducto = productosEJB.existeProducto(enlace); // Comprueba si el producto ya existe en la BBDD
+			/**
+			 * Si no existe lo añade en la tabla producto
+			 */
 			if (idProducto == null) {
 				productosEJB.insertProducto("Producto sin nombre", enlace, "imgs/productoSinImg.png");
 				idProducto = productosEJB.existeProducto(enlace);
 			}
 			Integer idLista = listasEJB.getIdLista(usuario.getId());
 			try {
-				listasEJB.addContenido(precioObjetivo, idLista, idProducto);
+				listasEJB.addContenido(precioObjetivo, idLista, idProducto); // Añade el contenido a la lista
 				response.sendRedirect("Lista");
 			} catch (NumberFormatException e) {
 				log.getLoggerAddProducto().error("Se ha producido un error en POST AddProducto: ", e);
--- src/aplicacion/controlador/AdminUser.java
+++ src/aplicacion/controlador/AdminUser.java
@@ -33,12 +33,19 @@ public class AdminUser extends HttpServlet {
 	UsuariosEJB usuariosEJB;
 
 	@Override
+	/****
+	 * Método GET que obtiene el usuario de la sesión y si está logueado muestra la
+	 * página de administrar usuario. Si no, redirige a Principal.
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession(false);
 		LogSingleton log = LogSingleton.getInstance();
 		Usuario usuario = sesionesEJB.usuarioLogeado(session);
+		/**
+		 * Si no está logueado redirige a Principal.
+		 */
 		if (usuario != null) {
 			RequestDispatcher rs = getServletContext().getRequestDispatcher("/PaginaAdminUser.jsp");
 			request.setAttribute("usuario", usuario);
@@ -53,19 +60,41 @@ public class AdminUser extends HttpServlet {
 	}
 
 	@Override
+	/****
+	 * Método POST que obtiene el usuario de la sesión, el nombre, correo,
+	 * contraseña actual, la nueva contraseña y la nueva contraseña repetida de los
+	 * parametros para modificar los datos del usuario logueado con estos.
+	 * 
+	 * Si el usuario ha sido modificado cierra la sesión con el usuario antiguo y
+	 * redirige a Login.
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession(false);
 		LogSingleton log = LogSingleton.getInstance();
+		/**
+		 * Obtiene el usuario de la sesión.
+		 */
 		Usuario usuarioLogueado = sesionesEJB.usuarioLogeado(session);
 		String error = null;
+		/**
+		 * Si el usuario está logueado modifica el usuario y redirige a Login o muestra
+		 * un mensaje de error. Si no, redirige a Principal.
+		 */
 		if (usuarioLogueado != null) {
+			/**
+			 * Obtiene los datos necesarios para modificar el usuario.
+			 */
 			String nombre = request.getParameter("nombre");
 			String correo = request.getParameter("correo");
 			String password = request.getParameter("password");
 			String password2 = request.getParameter("password2");
 			String confirmaPassword2 = request.getParameter("confirmaPassword2");
+			/**
+			 * Si obtiene todos los datos intenta modificar el usuario, si no muestra un
+			 * error.
+			 */
 			if (nombre != null & correo != null & password != null & password2 != null & confirmaPassword2 != null) {
 				/*
 				 * Obtenemos el usuario que tenga el correo y password original, si no lo
@@ -73,12 +102,33 @@ public class AdminUser extends HttpServlet {
 				 */
 				Usuario usuarioAModificar = usuariosEJB.loginUsuario(usuarioLogueado.getCorreo(), password);
 				boolean correoRepetido = false;
+				/**
+				 * Si el correo es diferente comprueba si el nuevo correo ya existe en la BBDD.
+				 */
 				if (!usuarioLogueado.getCorreo().equals(correo)) {
 					correoRepetido = usuariosEJB.existeUsuario(correo);
 				}
+				/**
+				 * Si el correo no está en la BBDD muestra un error. Si no, intenta modificar el
+				 * usuario.
+				 */
 				if (!correoRepetido) {
+					/**
+					 * Si el usuario obtenido a partir de los datos de sesión está vacío muestra un
+					 * mensaje de error. Si no, intenta modificar el usuario.
+					 */
 					if (usuarioAModificar != null) {
+						/**
+						 * Si las dos nuevas contraseñas no están en blanco intenta cambiar el usuario.
+						 * Si ninguna de las contraseñas están en blanco cambia el nombre y correo del
+						 * usuario, termina la sesión y redirige a Login. Si alguna de las nuevas
+						 * contraseñas no está vacía pero la otra sí muestra un mensaje de error.
+						 */
 						if (!password2.equals("") && !confirmaPassword2.equals("")) {
+							/**
+							 * Si las nuevas contraseñas coinciden edita el usuario, cierra la sesión y
+							 * redirige a Login.
+							 */
 							if (password2.equals(confirmaPassword2)) {
 								usuariosEJB.editarUsuario(usuarioAModificar.getCorreo(), nombre, correo, password2);
 								sesionesEJB.logoutUsuario(session);
@@ -104,6 +154,9 @@ public class AdminUser extends HttpServlet {
 			} else {
 				error = FALTAN_DATOS;
 			}
+			/**
+			 * Si ha ocurrido un error muestra la página AdminUser con el error
+			 */
 			if (error != null) {
 				RequestDispatcher rs = getServletContext().getRequestDispatcher("/PaginaAdminUser.jsp");
 
